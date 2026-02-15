e7601802abc969b45aaf91961bdc32be438e3c3f
Dobby <puithiraporn@gmail.com>
2026-02-15 19:43:20 +0700

    remove comment3

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -8,8 +8,7 @@ const AuthenticationForm = () => {
         console.log("Login from test-branch");
         console.log("Login from test-branch2");
         console.log("Login from new-branch");
-        console.log("######TEST######")
-        console.log("Tes ABCD");
+        console.log("######TEST######");
         
     }
     return (
